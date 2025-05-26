1ee6e4b5a2316cb44561754888edb65df76b1727
naveen-2111-dev <naveenrajanm9@gmail.com>
2025-05-26 10:51:14 -0700

    ssr

--- config/providers/Privy-Provider.tsx
+++ config/providers/Privy-Provider.tsx
@@ -1,8 +1,20 @@
 "use client";
 
 import { PrivyProvider } from '@privy-io/react-auth';
+import { useEffect, useState } from 'react';
 
 export const Privyprovider = ({ children }: { children: React.ReactNode }) => {
+
+    const [isClient, setIsClient] = useState(false);
+    const appId = process.env.NEXT_PUBLIC_PRIVY_APP_ID;
+
+    useEffect(() => {
+        setIsClient(true);
+        console.log("PrivyProvider initialized with appId:", appId);
+    }, []);
+
+    if (!isClient || !appId) return <>{children}</>;
+
     return (
         <PrivyProvider
             appId={process.env.NEXT_PUBLIC_PRIVY_APP_ID || ""}
